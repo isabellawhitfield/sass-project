82f1007070cdd0a35aaac8d0d17caeedfa642b2a
isabellawhitfield <53159435+isabellawhitfield@users.noreply.github.com>
2019-08-15 15:23:36 +1200

    Add files via upload

---
+++ sass/js/script.js
@@ -0,0 +1,23 @@
+console.log('my script is working')
+
+
+
+
+
+$(document).ready(function(){
+  $('.fas').click(function(){
+    $('nav').toggleClass('active');
+  });
+  // $('#close').click(function(){
+  //   $('nav').hide();
+  // })
+});
+
+// $(document).ready(function(){
+// $('#menu').click(function(){
+//   $('nav').toggleClass('active');
+// })
+// $('#close').click(function(){
+//   $('nav').hide();
+// })
+// })
